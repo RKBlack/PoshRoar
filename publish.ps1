d0d966dcbf3c8e4f11aa153fb36000976bb109b3
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2023-05-02 14:43:42 -0500

    folder filter

--- publish.ps1
+++ publish.ps1
@@ -11,20 +11,13 @@ $fileContent = Get-Content "$scriptPath\PoshRoar.psd1.source"
 $fileContent = $fileContent -replace '{{version}}', $version
 $fileContent = $fileContent -replace '{{CommitDate}}', $(Get-Date -Format 'yyyy-MM-dd')
 Set-Content "$scriptPath\PoshRoar\PoshRoar.psd1" -Value $fileContent -Force
-"`r"
-Get-Content "$scriptPath\PoshRoar\PoshRoar.psd1"
-"`r"
 
-Write-Output 'About to publish module'
+Write-Output 'Publishing module'
 $PublishParams = @{
     Path        = "$scriptPath\PoshRoar" 
     NuGetApiKey = $apiKey
     ProjectUri  = 'https://github.com/RKBlack/PoshRoar'
     LicenseUri  = 'https://github.com/RKBlack/PoshRoar/blob/main/LICENSE'
 }
-Write-Host "Path $($PublishParams.Path)"
-Get-ChildItem $PublishParams.Path
-Write-Host "ProjectUri $($PublishParams.ProjectUri)"
-Write-Host "LicenseUri $($PublishParams.LicenseUri)"
 
 Publish-Module @PublishParams -Force -Verbose
